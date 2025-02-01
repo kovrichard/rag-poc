4cdaa3770bed7b8c2117e52bd8b561354b8e1e23
Richard Kovacs <richardkovacs.it@gmail.com>
2025-02-01 12:59:30 +0100

    feat: add pdf and text support

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { type Request, type Response } from "express";
+import pdf from "pdf-parse";
 import { embedDocument } from "../dao/documents";
 
 export async function uploadHandler(req: Request, res: Response): Promise<void> {
@@ -8,10 +9,32 @@ export async function uploadHandler(req: Request, res: Response): Promise<void> 
     return;
   }
 
-  const { path } = req.file;
+  const { path, mimetype } = req.file;
+  let data: string;
 
-  const data = fs.readFileSync(path, "utf8");
+  if (mimetype === "text/plain") {
+    data = prepareTxtFile(path);
+  } else if (mimetype === "application/pdf") {
+    data = await preparePdfFile(path);
+  } else {
+    res.status(400).json({ message: "File type not supported" });
+    return;
+  }
+
   await embedDocument(data);
 
   res.json({ message: "File uploaded" });
+}
+
+function prepareTxtFile(path: string): string {
+  const data = fs.readFileSync(path, "utf8");
+
+  return data;
+}
+
+async function preparePdfFile(path: string): Promise<string> {
+  const file = fs.readFileSync(path);
+  const data = await pdf(file);
+
+  return data.text;
 }
