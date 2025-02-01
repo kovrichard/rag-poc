811fa28cb4c01ec77ef998a3ff2bff9609ee118c
Richard Kovacs <richardkovacs.it@gmail.com>
2025-02-01 10:30:33 +0100

    feat: only allow pdf and txt files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { type Request, type Response } from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
 import multer from "multer";
 import conf from "./lib/config";
 import { searchHandler } from "./routes/search";
@@ -10,7 +10,16 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  dest: "uploads/",
+  fileFilter: (_req: Request, file, cb) => {
+    if (file.mimetype !== "application/pdf" && file.mimetype !== "text/plain") {
+      cb(new Error("File type not supported"));
+    } else {
+      cb(null, true);
+    }
+  },
+});
 
 const app = express();
 
@@ -21,6 +30,10 @@ app.get("/search", searchHandler);
 
 app.use((_req: Request, res: Response) => {
   res.status(405).send("Method Not Allowed");
+});
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(400).json({ message: err.message });
 });
 
 app.listen(process.env.PORT, () => {
